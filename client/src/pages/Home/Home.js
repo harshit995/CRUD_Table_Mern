aafd9ca08aaab635ee9871decca1a19d80a37093
Harshit <gharshit89@gmail.com>
2023-01-28 05:00:04 +0530

    update

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,15 +3,23 @@ import "./home.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Dropdown from "react-bootstrap/Dropdown";
+
 import Row from "react-bootstrap/Row";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const adduser = () => {
+    navigate("/register");
+  };
   return (
     <>
       <div className="container">
         <div className="main_div">
           {/* search add button */}
-          <div className="search_add mt-4 d-flex justify-content-between">
+          <div className=" search_add mt-4 d-flex justify-content-between">
             <div className="search col-lg-4">
               <Form className="d-flex">
                 <Form.Control
@@ -26,7 +34,7 @@ const Home = () => {
               </Form>
             </div>
             <div className="add_btn">
-              <Button variant="primary">
+              <Button variant="primary" onClick={adduser}>
                 <i class="fa-solid fa-plus"></i>&nbsp; Add User
               </Button>
             </div>
@@ -34,7 +42,7 @@ const Home = () => {
           {/* export,gender,status  */}
 
           <div className="filter_div mt-5 d-flex justify-content-between flex-wrap">
-            <div className="export_csv">
+            <div className="export_csv p-3">
               <Button variant="primary" className="export_btn">
                 <i class="fa-solid fa-file-export"></i>&nbsp; Export csv
               </Button>
@@ -42,7 +50,73 @@ const Home = () => {
             <div className="filter_gender">
               <div className="filter">
                 <h3>Filter by Gender</h3>
-                <div className="gender d-flex justify-content-around"></div>
+                <div className="gender d-flex justify-content-around">
+                  <Form.Check
+                    type={"radio"}
+                    label={"All"}
+                    name="gender"
+                    value={"All"}
+                    defaultChecked
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={"Male"}
+                    name="gender"
+                    value={"Male"}
+                  />
+
+                  <Form.Check
+                    type={"radio"}
+                    label={"Female"}
+                    name="gender"
+                    value={"Female"}
+                  />
+                </div>
+              </div>
+            </div>
+            {/* sort by value  */}
+
+            <div className="filter_newold ">
+              <h3>Sort By value &nbsp;</h3>
+              <Dropdown className="text-center">
+                <Dropdown.Toggle id="dropdown-basic">
+                  <i class="fa-solid fa-sort"></i>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item>New</Dropdown.Item>
+                  <Dropdown.Item>Old</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+
+            {/* by status */}
+            <div className="filter_status">
+              <div className="status">
+                <h3>Filter By Status</h3>
+                <div className="status_radio d-flex justify-content-between flex-wrap">
+                  <Form.Check
+                    type={"radio"}
+                    label={`All`}
+                    name="status"
+                    value={"All"}
+                    // onChange={(e) => setStatus(e.target.value)}
+                    defaultChecked
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={`Active`}
+                    name="status"
+                    value={"Active"}
+                    // onChange={(e) => setStatus(e.target.value)}
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={`InActive`}
+                    name="status"
+                    value={"InActive"}
+                    // onChange={(e) => setStatus(e.target.value)}
+                  />
+                </div>
               </div>
             </div>
           </div>
