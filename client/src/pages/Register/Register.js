439df470e2e1065d4cc396836ea3173baedbd223
Harshit <gharshit89@gmail.com>
2023-01-28 02:30:36 +0530

    update

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,45 +1,124 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./register.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Select from "react-select";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
-  const [inputdata, setInputdata] = useState({});
+  const [inputdata, setInputdata] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    mobile: "",
+    gender: "",
+    location: "",
+  });
+
+  const [status, setStatus] = useState("Active");
+  const [image, setImage] = useState("");
+  const [preview, setPreview] = useState("");
 
   //status options
   const options = [
     { value: "Active", label: "Active" },
     { value: "InActive", label: "InActive" },
   ];
+
+  //setinput valu3e
+  const setInputValue = (e) => {
+    const { name, value } = e.target;
+
+    setInputdata({ ...inputdata, [name]: value });
+  };
+
+  // status set
+  const setStatusValue = (e) => {
+    setStatus(e.value);
+  };
+
+  //profile
+  const setprofile = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  //submit user data
+  const submitUserData = (e) => {
+    e.preventDefault();
+
+    const { fname, lname, email, mobile, gender, location } = inputdata;
+
+    if (
+      fname === "" ||
+      lname === "" ||
+      email === "" ||
+      mobile === "" ||
+      gender === "" ||
+      location === ""
+    ) {
+      toast.error("All fields are required");
+    } else if (!email.includes("@")) {
+      toast.error("Enter Valid Email");
+    } else if (mobile.length > 10) {
+      toast.error("Enter Valid Mobile number");
+    } else {
+      toast.success("Registeration is successfully done!");
+    }
+  };
+
+  //setpreview
+  useEffect(() => {
+    if (image) {
+      setPreview(URL.createObjectURL(image));
+    }
+  }, [image]);
+
   return (
     <>
       <div className="container">
         <h2 className="text-center mt-1">Registeration</h2>
         <Card className="shadow mt-3 p-3">
           <div className="profile_div text-center">
-            <img src="/man.png" alt="img" />
+            <img src={preview ? preview : "./man.png"} alt="img" />
           </div>
 
           <Form>
             <Row>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" name="fname" />
+                <Form.Control
+                  onChange={setInputValue}
+                  type="text"
+                  name="fname"
+                />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" name="lname" />
+                <Form.Control
+                  onChange={setInputValue}
+                  type="text"
+                  name="lname"
+                />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" name="email" />
+                <Form.Control
+                  onChange={setInputValue}
+                  type="email"
+                  name="email"
+                />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>Mobile</Form.Label>
-                <Form.Control type="text" name="mobile" />
+                <Form.Control
+                  onChange={setInputValue}
+                  type="text"
+                  name="mobile"
+                />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>Select your Gender</Form.Label>
@@ -48,6 +127,7 @@ const Register = () => {
                   label={"Male"}
                   name="gender"
                   value={"Male"}
+                  onChange={setInputValue}
                 />
 
                 <Form.Check
@@ -55,26 +135,36 @@ const Register = () => {
                   label={"Female"}
                   name="gender"
                   value={"Female"}
+                  onChange={setInputValue}
                 />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>Select your Status</Form.Label>
-                <Select options={options} />
+                <Select options={options} onChange={setStatusValue} />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>Select Your Profile</Form.Label>
-                <Form.Control type="file" name="user_profile" />
+                <Form.Control
+                  type="file"
+                  onChange={setprofile}
+                  name="user_profile"
+                />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
                 <Form.Label>Enter Your Location</Form.Label>
-                <Form.Control type="text" name="location" />
+                <Form.Control
+                  onChange={setInputValue}
+                  type="text"
+                  name="location"
+                />
               </Form.Group>
-              <Button variant="primary" type="submit">
+              <Button variant="primary" type="submit" onClick={submitUserData}>
                 Submit
               </Button>
             </Row>
           </Form>
         </Card>
+        <ToastContainer position="top-center" />
       </div>
     </>
   );
